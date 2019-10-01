8c413fe1862ea029d67ea08a20c60cd7919974a8
Nicole El-kaseeh <nicoleelks1@gmail.com>
2019-09-30 22:15:13 -0500

    owo

--- p5/Week6SoundIn/sketch.js
+++ p5/Week6SoundIn/sketch.js
@@ -1,6 +1,6 @@
 var mic;
 var vol;
-var x = 0 ;
+var y = 0 ;
 
 function setup() {
   createCanvas(800, 800);
@@ -15,18 +15,18 @@ let vol = mic.getLevel();
 fill('yellow');
 stroke(0);
 
-let h = map(vol, 0, .2, height, 0);
+let h = map(vol, 0, .5, height, 0);
 let c = map(vol, 0, 1, 0, 500);
 fill(c);
 background(c, 0, 0);
-ellipse(x, 200, 100, 50) ;
+ellipse(200, y, 100, 50) ;
   vol = mic.getLevel(); // level is between 0 and 1
   vol = vol * 100; // you may need to change this
 
-  if (x > width-200) {
-     x = -400 ;
+  if (y > width-200) {
+     y = -400 ;
   }
-    x++ ;
+    y++ ;
 
   }
 
