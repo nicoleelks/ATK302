2ecb89f5d1417fd2d50bc766cdf0c2a1333a1e0a
Nicole El-kaseeh <nicoleelks1@gmail.com>
2019-10-05 19:27:03 -0500

    jkjh

--- p5/mp22/sketch.js
+++ p5/mp22/sketch.js
@@ -1,21 +1,23 @@
 var myState = 0;
-//var myTimer = 10;
-var x = 0;
-var velocity = 3;
 var timer = 200;
+var x = 0;
+var velocity = 5;
+
 
 function setup() {
+  // put setup code here
   createCanvas(800, 800);
-
+  //rectMode(CENTER);
+  //ellipseMode(CENTER);
 }
-
 
 function draw() {
   // put drawing code here
+  background(100);
 
   switch (myState) {
+    case 0: // green state
 
-    case 0:
       fill('#875729');
       rect(255, 517, 100, 175);
 
@@ -79,17 +81,11 @@ function draw() {
       fill('#d6a676');
       rect(300, 610, 10, 90);
 
-      //  myTimer++;
-      //  if (myTimer >= 100) {
-      //  myState = 1;
-      //  myTimer = 0;
-      //  }
-
-
 
       break;
 
-    case 1:
+    case 1: // green state
+
       fill('#875729');
       rect(255, 517, 100, 175);
 
@@ -121,10 +117,10 @@ function draw() {
       ellipse(344, 342, 40, 50);
 
       fill('black');
-      ellipse(250, 350, 20, 30);
+      ellipse(253, 341, 20, 30);
 
       fill('black');
-      ellipse(340, 350, 20, 30);
+      ellipse(344, 342, 20, 30);
 
       fill('pink');
       triangle(197, 276, 189, 317, 222, 289);
@@ -152,19 +148,13 @@ function draw() {
 
       fill('#d6a676');
       rect(300, 610, 10, 90);
-//mouse
-      fill('black');
-      arc(50, 700, 100, 100, PI, TWO_PI);
 
-      fill('brown');
-      ellipse(100, 680, 50, 40);
+      doTimer(1, 200);
+      velocity = 5;
+      break;
 
+    case 2: // yellow state      fill('grey');
 
-
-
-
-
-    case 2:
       fill('#875729');
       rect(255, 517, 100, 175);
 
@@ -196,10 +186,78 @@ function draw() {
       ellipse(344, 342, 40, 50);
 
       fill('black');
-      ellipse(250, 350, 20, 30);
+      ellipse(253, 350, 20, 30);
 
       fill('black');
-      ellipse(340, 350, 20, 30);
+      ellipse(343, 350, 20, 30);
+
+      fill('pink');
+      triangle(197, 276, 189, 317, 222, 289);
+
+      fill('pink');
+      triangle(428, 267, 395, 288, 431, 321);
+
+      fill('#d6a676');
+      arc(370, 700, 100, 100, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(240, 700, 100, 100, PI, TWO_PI);
+
+      fill('black');
+      arc(270, 700, 70, 70, PI, TWO_PI);
+
+      fill('black');
+      arc(340, 700, 70, 70, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(264, 600, 35, 55, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(350, 600, 35, 55, PI, TWO_PI);
+
+      fill('#d6a676');
+      rect(300, 610, 10, 90);
+
+      doTimer(2, 100);
+      velocity = 2;
+      break;
+
+    case 3: // red state
+      fill('#875729');
+      rect(255, 517, 100, 175);
+
+      fill('#d6a676');
+      rect(247, 600, 35, 70);
+
+      fill('#d6a676');
+      rect(333, 600, 35, 70);
+
+      fill('black');
+      arc(370, 700, 50, 50, PI, TWO_PI);
+
+      fill('#ba8857');
+      ellipse(310, 400, 300, 250);
+
+      fill('brown');
+      triangle(189, 246, 167, 356, 249, 286);
+
+      fill('brown');
+      triangle(439, 242, 368, 286, 449, 352);
+
+      fill('brown');
+      triangle(299, 442, 267, 397, 338, 398);
+
+      fill('#a3fffa');
+      ellipse(253, 341, 40, 50);
+
+      fill('#a3fffa');
+      ellipse(344, 342, 40, 50);
+
+      fill('black');
+      ellipse(255, 350, 20, 30);
+
+      fill('black');
+      ellipse(345, 350, 20, 30);
 
       fill('black');
       triangle(280, 280, 250, 300, 290, 370);
@@ -234,69 +292,110 @@ function draw() {
 
       fill('#d6a676');
       rect(300, 610, 10, 90);
-//mouse
+
+      doTimer(2, 100);
+      velocity = 0;
+      break;
+
+    case 4:
+      fill('#875729');
+      rect(255, 517, 100, 175);
+
+      fill('#d6a676');
+      rect(247, 600, 35, 70);
+
+      fill('#d6a676');
+      rect(333, 600, 35, 70);
+
       fill('black');
-      arc(50, 700, 100, 100, PI, TWO_PI);
+      arc(370, 700, 50, 50, PI, TWO_PI);
+
+      fill('#ba8857');
+      ellipse(310, 400, 300, 250);
 
       fill('brown');
-      ellipse(100, 680, 50, 40);
+      triangle(189, 246, 167, 356, 249, 286);
+
+      fill('brown');
+      triangle(439, 242, 368, 286, 449, 352);
+
+      fill('brown');
+      triangle(299, 442, 267, 397, 338, 398);
+
+      fill('#a3fffa');
+      ellipse(253, 341, 40, 50);
+
+      fill('#a3fffa');
+      ellipse(344, 342, 40, 50);
+
+      fill('black');
+      ellipse(255, 350, 20, 30);
+
+      fill('black');
+      ellipse(345, 350, 20, 30);
+
+      fill('black');
+      triangle(280, 280, 250, 300, 290, 370);
+
+      fill('black');
+      triangle(320, 280, 350, 300, 300, 370);
 
 
+      fill('pink');
+      triangle(197, 276, 189, 317, 222, 289);
 
+      fill('pink');
+      triangle(428, 267, 395, 288, 431, 321);
+
+      fill('#d6a676');
+      arc(370, 700, 100, 100, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(240, 700, 100, 100, PI, TWO_PI);
+
+      fill('black');
+      arc(270, 700, 70, 70, PI, TWO_PI);
+
+      fill('black');
+      arc(340, 700, 70, 70, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(264, 600, 35, 55, PI, TWO_PI);
+
+      fill('#d6a676');
+      arc(350, 600, 35, 55, PI, TWO_PI);
+
+      fill('#d6a676');
+      rect(300, 610, 10, 90);
 
       break;
 
-    case 3:
-      //fill('black');
-      //arc(x, 700, 100, 100, PI, TWO_PI);
-      //fill('brown');
-    //  ellipse(x, 680, 50, 40);
-
-      //doTimer(1, 200);
-      //velocity = 50;
-      //x = x + velocity;
-      //if (x > width) {
-      //  x = 0;
-      //}
-
-      //myTimer++;
-      //if (myTimer >= 100) {
-      //myState = 4;
-      // myTimer = 0;
-      //}
-      break;
   }
 
-
-fill('black');
-//mouse
-arc(x, 700, 100, 100, PI, TWO_PI);
-fill('brown');
-ellipse(x, 680, 50, 40);
-x = x + velocity;
-if (x > width) {
-  x = 0;
-}
+  fill('black');
+  arc(x, 700, 100, 100, PI, TWO_PI);
+  fill('brown');
+  ellipse(x + 45, 680, 50, 40);
+  x = x + velocity;
+  if (x > width) {
+    x = 0;
+  }
 
 }
 
 
-//function doTimer(variableState, variableTimer) {
-  //timer--;
-  //if (timer <= 0) {
-    //timer = variableTimer;
-    //myState = variableState;
-  //}
-//}
 
 function mouseReleased() {
   myState += 1;
-  if (myState > 3) {
+  if (myState > 4) {
     myState = 0;
   }
-
 }
 
-
-
-// you need to click the screen b
+function doTimer(variableState, variableTimer) {
+  timer--;
+  if (timer <= 0) {
+    timer = variableTimer;
+    myState = variableState;
+  }
+}
